bbbf338037b00cdb1d1203f422de1823e151e228
leaddesigner63 <alex.klim63@gmail.com>
2026-02-03 21:53:26 +0300

    Resolve Alembic multiple heads

---
+++ alembic/versions/0007_merge_profile_text_fields_heads.py
@@ -0,0 +1,19 @@
+"""merge profile text fields heads
+
+Revision ID: 0007_merge_profile_text_fields_heads
+Revises: 0006_expand_user_profile_text_fields, 0006_profile_text_fields
+Create Date: 2026-02-03 00:00:00.000000
+"""
+
+revision = "0007_merge_profile_text_fields_heads"
+down_revision = ("0006_expand_user_profile_text_fields", "0006_profile_text_fields")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
